5fe65a46f1d9042def20e4323e3823518e23ee78
Ivan Gimenez <ivanlgimenez@hotmail.com>
2025-06-24 20:24:33 -0300

    Correciones en el archivo index, se añadio la base

--- myapp/views.py
+++ myapp/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
+    from datetime import datetime
+    time_actual = datetime.now()
     contexto = {
-        "mensaje": "Espero que aqui encuentres todo lo que necesitas."
+        "mensaje": "Espero que aqui encuentres todo lo que necesitas. Aqui vas a encontrar todas las herramientas que necesites para que tu emprendimiento sea exitoso.",
+        "time_actual": time_actual
     }
-    return render(request, "myapp/index.html", contexto)
+    return render(request, "myapp/index.html", context=contexto)
     
